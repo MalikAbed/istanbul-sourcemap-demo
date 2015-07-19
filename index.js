d979f9cb75360eaa563f8fd85e9539e933c2c4a4
Malik Abed <malik.1does@gmail.com>
2015-07-19 23:07:40 +0200

    Update test file

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-class Dog {
+var evens = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var fives = [];
+// Expression bodies
+var odds = evens.map(v => v + 1);
+var nums = evens.map((v, i) => v + i);
+
+// Statement bodies
+nums.forEach(v => {
+  if (v % 5 === 0)
+    fives.push(v);
+});
+
+console.log("Odds", odds);
+console.log("nums", nums);
+console.log("Fives", fives);
+
+// Lexical this
+var malik = {
+  _name: "Malik",
+  _friends: ["Douglas"],
+  printFriends() {
+    this._friends.forEach(f =>
+      console.log(this._name + " knows " + f));
+  }
 }
 
-// const snoop = new Dog();
-// snoop.sit()
-
+malik.printFriends();
